c74b34d7ea5b6640c323625700cec2134f86daf4
Stefan Weck <stefanweck1@gmail.com>
2015-07-09 20:13:07 +0200

    Added Gulp and other javascript goodies.

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+//All dependencies loaded
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+//Folder paths
+var jsBuildPath = 'js/dist';
+
+//Javascript files list
+var javascriptFiles = [
+    'js/src/main.js'
+];
+
+/**
+ * Build for production, without sourcemaps and uglified
+ * Runs clean before running this
+ */
+gulp.task('build-js', function ()
+{
+    return gulp.src(javascriptFiles)
+        .pipe(uglify())
+        .pipe(concat('final.min.js'))
+        .pipe(gulp.dest(jsBuildPath));
+});
+
+/**
+ * Default watcher for development. Cleans build folder before and does a build before starting watching
+ */
+gulp.task('default', function ()
+{
+    gulp.start('build-js');
+
+    gulp.watch(javascriptFiles, ['build-js']);
+});
