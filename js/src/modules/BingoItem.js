7bcc56dad32e9938ea7b4e3417c79bdc1c012c9c
antwan <antwan@antwan.eu>
2015-07-09 21:15:58 +0200

    Added click event for strike through

--- js/src/modules/BingoItem.js
+++ js/src/modules/BingoItem.js
@@ -2,9 +2,19 @@ var BingoItem = React.createClass({
     render: function ()
     {
         return (
-            <div className="bingoItem">
+            <div className="bingoItem" onClick={this.clickCard}>
                 {this.props.string}
             </div>
         );
+    },
+
+    /**
+     * @param e
+     */
+    clickCard: function (e)
+    {
+        e.target.classList.add('strike-through');
+
+        //@TODO Notifications
     }
 });
