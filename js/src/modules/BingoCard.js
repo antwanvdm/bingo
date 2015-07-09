2aa7a1aedd67e845130e77d23b2a5eae5e8900f1
Stefan Weck <stefanweck1@gmail.com>
2015-07-09 21:27:50 +0200

    Cleaned up my crap.... sorry world.

--- js/src/modules/BingoCard.js
+++ js/src/modules/BingoCard.js
@@ -7,12 +7,13 @@ var BingoCard = React.createClass({
             </div>
         );
     },
-    getBingoItems: function()
+    
+    getBingoItems: function ()
     {
         var itemStack = items;
         var bingoItems = [];
 
-        for(var i = 0; i < 16; i++){
+        for (var i = 0; i < 16; i++) {
 
             var randomNumber = this.randomNumber(0, itemStack.length - 1);
             var randomItem = itemStack.splice(randomNumber, 1);
@@ -23,7 +24,8 @@ var BingoCard = React.createClass({
 
         return bingoItems;
     },
-    randomNumber: function(from, to)
+
+    randomNumber: function (from, to)
     {
         return Math.floor(Math.random() * (to - from + 1) + from);
     }
